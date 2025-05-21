45b9985ba2ddd903392f964b1b1870439ba1187d
Maicon Santana <maiconpintoabreu@gmail.com>
2025-05-21 20:20:15 +0000

    Add recomendations and set fps to 60

--- src/main.zig
+++ src/main.zig
@@ -3,9 +3,10 @@ const gameLogic = @import("game.zig");
 
 pub fn main() anyerror!void {
     rl.traceLog(rl.TraceLogLevel.info, "Initializing Game!", .{});
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    gameLogic.startGame();
     defer rl.closeWindow(); // Close window and OpenGL context
+
+    rl.setTargetFPS(60);
+    gameLogic.startGame();
+
     while (gameLogic.updateFrame()) {}
 }
